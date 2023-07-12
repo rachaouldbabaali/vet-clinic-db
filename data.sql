c82995bfcd22d2d95595039dcae35201eea0b134
rachaouldbabaali <rachaould@gmail.com>
2023-07-12 10:23:01 +0100

    Update data base and do the changes

--- data.sql
+++ data.sql
@@ -23,26 +23,29 @@ vet_clinic=# select * from animals;
 (4 rows)
 
 */
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutred, weight_kg, species)
-VALUES (5, 'Charmander', '2020-02-08', 0, false, -11.0, 'Fire Lizard');
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutred, weight_kg, species)
-VALUES (6, 'Plantmon', '2021-11-15', 2, true, -5.7, 'Plant');
+ALTER TABLE animals ADD species varchar(100);
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutred, weight_kg, species)
-VALUES (7, 'Squirtle', '1993-04-02', 3, false, -12.13, 'Water Turtle');
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutred, weight_kg)
+VALUES (5, 'Charmander', '2020-02-08', 0, false, -11.0);
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutred, weight_kg, species)
-VALUES (8, 'Angemon', '2005-06-12', 1, true, -45.0, 'Angel');
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutred, weight_kg)
+VALUES (6, 'Plantmon', '2021-11-15', 2, true, -5.7);
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutred, weight_kg, species)
-VALUES (9, 'Boarmon', '2005-06-07', 7, true, 20.4, 'Boar');
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutred, weight_kg)
+VALUES (7, 'Squirtle', '1993-04-02', 3, false, -12.13);
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutred, weight_kg, species)
-VALUES (10, 'Blossom', '1998-10-13', 3, true, 17.0, 'Flower');
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutred, weight_kg)
+VALUES (8, 'Angemon', '2005-06-12', 1, true, -45.0);
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutred, weight_kg, species)
-VALUES (11, 'Ditto', '2022-05-14', 4, true, 22.0, 'Transforming');
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutred, weight_kg)
+VALUES (9, 'Boarmon', '2005-06-07', 7, true, 20.4);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutred, weight_kg)
+VALUES (10, 'Blossom', '1998-10-13', 3, true, 17.0);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutred, weight_kg)
+VALUES (11, 'Ditto', '2022-05-14', 4, true, 22.0);
 
 /* 
  id |    name    | date_of_birth | escape_attempts | neutred | weight_kg |   species
@@ -51,13 +54,13 @@ VALUES (11, 'Ditto', '2022-05-14', 4, true, 22.0, 'Transforming');
   2 | Gabumon    | 2018-11-15    |               2 | t       |         8 |
   3 | Pikachu    | 2021-01-07    |               1 | f       |     15.04 |
   4 | Devimon    | 2017-05-12    |               5 | t       |        11 |
-  5 | Charmander | 2020-02-08    |               0 | f       |       -11 | Fire Lizard
-  6 | Plantmon   | 2021-11-15    |               2 | t       |      -5.7 | Plant
-  7 | Squirtle   | 1993-04-02    |               3 | f       |    -12.13 | Water Turtle
-  8 | Angemon    | 2005-06-12    |               1 | t       |       -45 | Angel
-  9 | Boarmon    | 2005-06-07    |               7 | t       |      20.4 | Boar
- 10 | Blossom    | 1998-10-13    |               3 | t       |        17 | Flower
- 11 | Ditto      | 2022-05-14    |               4 | t       |        22 | Transforming
+  5 | Charmander | 2020-02-08    |               0 | f       |       -11 | 
+  6 | Plantmon   | 2021-11-15    |               2 | t       |      -5.7 | 
+  7 | Squirtle   | 1993-04-02    |               3 | f       |    -12.13 | 
+  8 | Angemon    | 2005-06-12    |               1 | t       |       -45 | 
+  9 | Boarmon    | 2005-06-07    |               7 | t       |      20.4 | 
+ 10 | Blossom    | 1998-10-13    |               3 | t       |        17 | 
+ 11 | Ditto      | 2022-05-14    |               4 | t       |        22 | 
 (11 rows)
     
     */  
--- schema.sql
+++ schema.sql
@@ -6,7 +6,6 @@ CREATE TABLE animals (
     date_of_birth date,
     escape_attempts int,
     neutred boolean,
-    weight_kg DECIMAL(10,2),
-    species varchar(100)
+    weight_kg DECIMAL(10,2)
 );
 
