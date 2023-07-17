2c4fac6d1a2e1d17ade8df5e91def6ea592e1476
rachaouldbabaali <rachaould@gmail.com>
2023-07-17 19:31:04 +0100

    Add more data

--- schema.sql
+++ schema.sql
@@ -46,3 +46,12 @@ CREATE TABLE visits(
     animals_id INT REFERENCES animals(id),
     visit_date DATE
 );
+
+
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+INSERT INTO visits (animals_id, vets_id, visit_date) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
+
+-- excution time: 343 ms so we re insert the data
+INSERT INTO visits (animals_id, vets_id, visit_date) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
